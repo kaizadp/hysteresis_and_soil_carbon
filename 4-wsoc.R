9a1de9ee32bdecda8d2307e9f5cfe27c25d6df95
Kaizad <kaizad.patel@pnnl.gov>
2019-11-21 17:45:08 -0800

    made drake plan

--- 4-wsoc.R
+++ 4-wsoc.R
@@ -12,24 +12,20 @@
 source("0-hysteresis_packages.R")
 library(purrr)
 
+
+wsoc_plan = drake_plan(
+  
 # step 1. import all files ---------------------------- ####
 
 # import and merge all data files in the folder
-wsoc_raw = sapply(list.files(path = "data/wsoc_data/",pattern = "*.csv", full.names = TRUE) ,
-                  read_csv, simplify = FALSE) %>% bind_rows()
+wsoc_raw = sapply(list.files(path = "data/wsoc_data/",pattern = "*.csv", full.names = TRUE),
+                  read_csv, simplify = FALSE) %>% bind_rows(),
+wsoc_key = read_csv("data/WSOC_SAMPLE_KEY.csv"), # wsoc key
+core_key = read.csv(COREKEY) %>% mutate(Core = as.character(Core)), # core key
 
-# wsoc key
-wsoc_key = read_csv("data/WSOC_SAMPLE_KEY.csv")
-
-# core key
-core_key = read_csv("data/Core_key.csv")
-core_key = read_excel("data/Core_key.xlsx")
-
-
-# get the names of the columns
-names(wsoc_raw)
-
-wsoc_raw %>% 
+# process
+wsoc_processed = 
+  wsoc_raw %>% 
 # remove the appropriate rows
 # in this case, we want Remove==NA and Excluded==1
   filter(is.na(Remove),
@@ -39,13 +35,12 @@ wsoc_raw %>%
 # the `Sample Name`` column is actually vial numbers, recorded as "Vial 1", "Vial 2", etc. Create a new column Vial_no by removing the "Vial " string at the start of the name.
   mutate(Vial_no = str_remove(`Sample Name`, "Vial ")) %>%
 # now combine with the wsoc_key
-  left_join(select(wsoc_key, -(DD:YYYY)), by = "Vial_no", all.x=T) -> wsoc_processed 
+  left_join(select(wsoc_key, -(DD:YYYY)), by = "Vial_no", all.x=T),
   
-
 # create a new file for just the calibration standards
 wsoc_calib = 
   wsoc_processed %>% 
-  filter(Type=="calibration")->  wsoc_calib
+  filter(Type=="calibration")->  wsoc_calib,
 
 
 #
@@ -58,27 +53,28 @@ gg_calib =
 #  facet_wrap(~Run)+
   ggtitle("Calibration curves")+
   ylab("NPOC, mg/L")+
-  theme_bw()
+  theme_bw(),
 
-save_plot("data/processed/wsoc_calibration_curves.tiff", gg_calib, base_height = 7, base_width = 7)
+save_plot("data/processed/wsoc_calibration_curves.tiff", gg_calib, base_height = 7, base_width = 7),
 
 # create new columns with the slope and intercept for each calibration curve
-wsoc_calib %>% 
+wsoc_calib_slope = 
+  wsoc_calib %>% 
  # dplyr::group_by(Run) %>% 
   dplyr::summarize(slope = lm(Calib_ppm~Area)$coefficients["Area"], 
-                   intercept = lm(Calib_ppm~Area)$coefficients["(Intercept)"])->
-  wsoc_calib_slope
+                   intercept = lm(Calib_ppm~Area)$coefficients["(Intercept)"]),
 
 ### if we are using a single calibration curve across all samples, then do this 
-SLOPE = mean(wsoc_calib_slope$slope)
-INTERCEPT = mean(wsoc_calib_slope$intercept)
+SLOPE = mean(wsoc_calib_slope$slope),
+INTERCEPT = mean(wsoc_calib_slope$intercept),
 
 #
 
 
 # step 3. calculate DOC concentrations ---------------------------- ####
 # first, create a file of only the sample data. no calibration or qaqc data
-wsoc_processed %>% 
+wsoc_samples = 
+  wsoc_processed %>% 
   filter(Type=="sample") %>% 
 # next calculate DOC as y = mx + c
   mutate(npoc = Area*SLOPE + INTERCEPT) %>% 
@@ -87,13 +83,36 @@ wsoc_processed %>%
 # subset only the relevant columns now
   dplyr::select(`Sample name`,npoc_mg_l) %>% 
 # rename so that we can merge easily later
-  rename(Core=`Sample name`)->
-  wsoc_samples
+  rename(Core=`Sample name`) %>% 
+  left_join(core_key, by="Core"),
+
+#
 
 
+# step 4. calculate as mg/g ---------------------------- ####
 
-write_csv(DOC_data_processed6_batch, path = "2_processeddata/doc_concentrations_batch.csv", na="")
-write_csv(DOC_data_processed6_column, path = "2_processeddata/doc_concentrations_column.csv", na="")
+# first, we need to retrieve weights of soil used for the extraction
+wsoc_weights = 
+  read_excel("data/Subsampling_weights.xlsx", sheet = "subsampling") %>% 
+  dplyr::select(Core_No, Moisture_perc, WSOC_weighed_g) %>% 
+  dplyr::rename(gmoist = Moisture_perc,
+                Core=Core_No) %>% 
+  dplyr::mutate(WSOC_drywt_g  = round(WSOC_weighed_g/((gmoist/100)+1),2),
+                WSOC_water_g = WSOC_weighed_g - WSOC_drywt_g),
+
+wsoc_results = wsoc_samples %>% 
+  left_join(wsoc_weights, by = "Core") %>% 
+  dplyr::mutate(wsoc_mg_g = round(npoc_mg_l * (40+WSOC_water_g)/(WSOC_drywt_g*1000),3)) %>% 
+  dplyr::select(Core,soil_type, texture, treatment, moisture_lvl, perc_sat, npoc_mg_l, wsoc_mg_g),
+
+write.csv(wsoc_results, WSOC, row.names = F, na=""))
+
+message("Now type: make(wsoc_plan)")
 
 
-
+WSOC = readd(WSOC)
+ggplot(WSOC, aes(x = perc_sat, y = wsoc_mg_g, color = treatment))+
+  geom_point()+
+  facet_wrap(~soil_type)+
+  scale_x_reverse(name = "percent saturation")
+  
