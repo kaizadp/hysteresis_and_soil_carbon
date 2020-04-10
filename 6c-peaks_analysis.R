6b05c96f9729da7dfc75bdf68f7fba0b9434725f
Kaizad <kaizad.patel@pnnl.gov>
2020-04-10 10:43:18 -0700

    Update 6c-peaks_analysis.R

--- 6c-peaks_analysis.R
+++ 6c-peaks_analysis.R
@@ -8,8 +8,8 @@ corekey_subset =
   dplyr::select(Core, texture, treatment, perc_sat, sat_level,Core_assignment) %>% 
   dplyr::mutate(Core = as.factor(Core))
 
-
-# frequency of peaks ----
+#
+# 1. frequency of peaks ----
 peaks2 = 
   peaks %>%
   dplyr::mutate(Core = as.character(Core)) %>% 
@@ -39,36 +39,7 @@ gg_nmr+
 
 #
 
-## peak assignment ----
-peak_assg = read.csv("data/nmr_peak_assignments.csv")
+# 2. peak assignment ----
+# see markdown file
 
-p = 
-  ggplot()+
-  geom_point(data = peak_assg, aes(x = deltaH_ppm, y = 0.5, color = abbrev))+
-  geom_text(data = peak_assg, aes(x = deltaH_ppm, y = 0.3, color = abbrev), label = peak_assg$abbrev, angle = 90)+
-  geom_text(data = peak_assg, aes(x = deltaH_ppm, y = 0.7, color = abbrev), label = peak_assg$moiety, angle = 90)+
-  geom_rect(data=peak_assg, aes(xmin=deltaH_start, xmax=deltaH_stop, 
-                                ymin=-Inf, ymax=1.0, fill=abbrev),alpha=0.1)+
-  geom_rect(data=bins2, aes(xmin=start, xmax=stop, 
-                            ymin=-Inf, ymax=+Inf), fill = "white", color="grey70",alpha=0.1)+
-  
-  
-  ylim(-0.1,1)+
-  
-  annotate("text", label = "aliphatic", x = 1.4, y = -0.1)+
-  annotate("text", label = "O-alkyl", x = 3.5, y = -0.1)+
-  annotate("text", label = "alpha-H", x = 4.45, y = -0.1)+
-  annotate("text", label = "aromatic", x = 7, y = -0.1)+
-  annotate("text", label = "amide", x = 8.1, y = -0.1)
-  
-  
-p + scale_x_reverse(limits = c(2.5,0))
-p + scale_x_reverse(limits = c(6,2.5))
-p + scale_x_reverse(limits = c(10,6))
-
-  
-  
-  
-
-
-  
+#
