ab5edb1491a87a967624b3b5709f1790ccae2d3f
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-07-31 09:55:20 -0700

    update wrc graphs. calculate water release for drying curves.

--- code/2-hyprop.R
+++ code/2-hyprop.R
@@ -4,6 +4,7 @@
 
 source("code/0-hysteresis_packages.R")
 
+#
           ###  load files ----
 filePaths <- list.files(path = "data/wrc/vg_pdi",pattern = "*.xlsx", full.names = TRUE)
 
@@ -275,7 +276,7 @@ wrc_processed_unfit %>%
 
 # hyprop with wp4c -- july 9/29 -- vg ----------------------------------------------
 
-filePaths <- list.files(path = "data/wrc/vg_wp4c",pattern = "*.xlsx", full.names = TRUE)
+filePaths <- list.files(path = "data/wrc/vg_wp4c-07-30",pattern = "*.xlsx", full.names = TRUE)
 
 wrc <- do.call(rbind, lapply(filePaths, function(path) {
   df <- read_excel(path, sheet="Fitting-Retention Θ(pF)")
@@ -290,8 +291,10 @@ wrc_processed =
   dplyr::mutate(treatment = case_when(grepl("drying",source)~"drying",
                                       grepl("wetting",source)~"wetting"),
                 texture = case_when(grepl("scl",source)~"SCL",
-                                    grepl("sl",source)~"SL")) %>% 
-  group_by(texture) %>% 
+                                    grepl("sl",source)~"SL"),
+                grav_water_perc = case_when(texture=="SCL" ~ vol_water_perc/0.4,
+                                            texture=="SL" ~ vol_water_perc/0.5)) %>% 
+  group_by(texture, treatment) %>% 
   dplyr::mutate(kPa = round((10^pF)/10,2),
                 perc_sat = round((vol_water_perc/max(vol_water_perc))*100,2)) %>% 
   dplyr::filter(#kPa < 200,
@@ -299,23 +302,24 @@ wrc_processed =
 
 ##
 wrc_processed %>% 
-  dplyr::mutate(skip = case_when(grepl("sl_drying",source)~"skip",
-                               grepl("scl_drying",source)~"skip")) %>% 
-  filter(is.na(skip)) %>% 
-  ggplot(aes(x = kPa, y = perc_sat, color = treatment, linetype=treatment, group = source))+
+  #dplyr::mutate(skip = case_when(grepl("sl_drying",source)~"skip",
+  #                             grepl("scl_drying",source)~"skip")) %>% 
+  #filter(is.na(skip)) %>% 
+  ggplot(aes(x = kPa, y = grav_water_perc, color = treatment, linetype=treatment, group = source))+
   geom_path(size=1.5)+
   scale_color_manual(labels = c("drying","rewetting"), values = c("darkorange2","gray40"), na.translate=F)+
   scale_linetype_manual(labels = c("drying","rewetting"), values = c("solid","twodash"), na.translate=F)+
   
   facet_wrap(.~texture)+
   xlab ("tension, kPa (log scale)")+
-  ylab ("percent saturation")+
+  ylab ("gravimetric moisture (%)")+
   scale_x_continuous(trans = scales::log10_trans(), labels = scales::comma, 
                      sec.axis = sec_axis(~ .^-1 * 300, name = "pore size (um)", labels = scales::comma))+
   geom_hline(yintercept = 5, size=0.5, color = "grey80")+
   geom_hline(yintercept = 35, size=0.5, color = "grey80")+
   geom_hline(yintercept = 50, size=0.5, color = "grey80")+
   geom_hline(yintercept = 75, size=0.5, color = "grey80")+
+  #coord_flip()+
   theme_kp()+
   theme(panel.grid = element_blank())
 
@@ -327,15 +331,16 @@ wrc_processed %>%
                                  grepl("scl_drying",source)~"skip")) %>% 
   filter(is.na(skip)) %>% 
   filter(treatment=="drying") %>% 
-  ggplot(aes(x = kPa, y = perc_sat, color = texture, group = source))+
+  ggplot(aes(x = kPa, y = grav_water_perc, color = texture, group = source))+
   geom_path(size=1.5)+
   scale_color_manual(#labels = c("drying","rewetting"), 
                      values = c("darkorange2","gray40"), na.translate=F)+
   scale_linetype_manual(labels = c("drying","rewetting"), values = c("solid","twodash"), na.translate=F)+
   
   #facet_wrap(.~texture)+
-  xlab ("tension, kPa (log scale)")+
-  ylab ("percent saturation")+
+  labs (x = "tension, kPa (log scale)",
+        y = "gravimetric moisture (%)",
+        title = "drying curves only")+
   scale_x_continuous(trans = scales::log10_trans(), labels = scales::comma, 
                      sec.axis = sec_axis(~ .^-1 * 300, name = "pore size (um)", labels = scales::comma))+
   geom_hline(yintercept = 5, size=0.5, color = "grey80")+
@@ -444,155 +449,182 @@ write.csv(wrc_processed, "data/processed/wrc.csv", row.names = F)
 
 # water release -----------------------------------------------------------
 
+## determine the volume of water released with successive increase in suction
 wrc_processed = read.csv("data/processed/wrc.csv")
 
-# release = 
-#   wrc_processed %>% 
-#   group_by(texture, treatment) %>% 
-#   mutate(Diff = vol_water_perc - lag(vol_water_perc))
+          # release = 
+          #   wrc_processed %>% 
+          #   group_by(texture, treatment) %>% 
+          #   mutate(Diff = vol_water_perc - lag(vol_water_perc))
 
-release_scl_wetting = 
+
+## SCL-drying
+
+release_scl_drying = 
   wrc_processed %>% 
   select(vol_water_perc, treatment, texture, kPa) %>% 
-  filter(treatment %in% "wetting" & texture %in% "SCL") %>% 
-  mutate(pore_um = round(300/kPa,2),
-         x_bins = cut(pore_um, breaks = 300)) %>% 
+  # keep only the SCL drying data
+  filter(treatment %in% "drying" & texture %in% "SCL") %>% 
+  # calculate wet pore size using the Kelvin equation
+  mutate(pore_um = round(300/kPa,2)) %>% 
+  # for repeating kPa/pore, calculate mean of water content
+  group_by(pore_um, texture, treatment) %>% 
+  dplyr::summarise(vol_water_perc = round(mean(vol_water_perc),2)) %>% 
+  ungroup() %>% 
+  # then create bins in increments of 10 (0-3000, 300 breakpoints)
+  mutate(x_bins = cut(pore_um, breaks = 300)) %>% 
+  # split the bins into start and stop columns
   separate(x_bins, c("start", "stop"), sep = ",") %>% 
   mutate(stop = str_replace(stop, "]",""),
          stop = as.numeric(stop),
          start = stri_replace_all_fixed(start, "(",""),
          #         start = str_replace(start, "-",""),
-         start = as.numeric(start)
-  ) 
+         start = as.numeric(start)) %>% 
+  arrange(desc(pore_um))
 
-release_scl_wetting_summary  = 
-  release_scl_wetting %>% 
-  group_by(start, stop, treatment, texture) %>% 
-  summarize(vol_water = max(vol_water_perc)) 
-
-release_scl_wetting_bins = 
-  release_scl_wetting_summary %>% 
-  ungroup() %>% 
-  # arrange(desc(stop)) %>% 
-  mutate(Diff = vol_water - lag(vol_water),
-         Diff_perc = (Diff/max(vol_water))*100)  %>% 
+release_scl_drying_diff = 
+  release_scl_drying %>% 
+  # calculate sequential loss of water (lag)
+  mutate(Diff = (vol_water_perc - lag(vol_water_perc))*-1) %>% 
   na.omit() %>% 
-  #  mutate(bins = case_when(start==50 & stop==100 ~ "50-100",
-  #                          start>=100 & start <250 ~ "100-250",
-  #                          start >=250 ~ "> 250")) %>% 
-  mutate(bins = if_else(stop<=100, stop, 3000)) %>% 
-  group_by(bins) %>% 
-  summarize(water_vol_perc = round(sum(Diff_perc),2))
+  add_row(texture="SCL", treatment="drying", 
+          Diff = min(release_scl_drying$vol_water_perc),
+          start = min(release_scl_drying$start),
+          stop = min(release_scl_drying$stop)) %>% 
+  mutate(Diff_perc = round((Diff/max(vol_water_perc, na.rm = TRUE))*100,2))
+  # ungroup() %>% 
+  # summarize(total = sum(Diff))
 
+        release_scl_drying_diff %>% na.omit() %>% 
+          ggplot()+
+          geom_point(aes(x = pore_um, y = Diff))
 
-bins_10 = tribble(
-  ~bins, ~water_vol_perc,
-  10, (33.43/max(release_scl_wetting_summary$vol_water))*100
-)
-## < 10 um = 37.43 = 51.54 % of total volume
+release_scl_drying_summary  = 
+  release_scl_drying_diff %>% 
+  group_by(start, stop, treatment, texture) %>% 
+  summarize(vol_water = sum(Diff)) 
 
-bins_combined = 
-  release_scl_wetting_bins %>% 
-  rbind(bins_10) %>% 
-  arrange(bins)
-
-ggplot(bins_combined, aes(x = as.character(bins), y = water_vol_perc))+
-  geom_point()
-
-
-release_scl_wetting_bins2 = 
-  release_scl_wetting %>% 
+release_scl_drying_bins = 
+  release_scl_drying_summary %>% 
   ungroup() %>% 
-  arrange((vol_water_perc)) %>% 
-  mutate(Diff = vol_water_perc - lag(vol_water_perc),
-         Diff_perc = (Diff/max(vol_water_perc))*100)  %>% 
+  arrange(desc(stop)) #%>% 
+  mutate(bins = if_else(stop<=100, stop, 3000)) %>% 
+  group_by(treatment, texture, bins) %>% 
+  dplyr::summarise(vol_water_perc = sum(vol_water))
+
+
+ggplot(release_scl_drying_bins, aes(x = stop, y = vol_water))+
+  geom_point()+
+  scale_x_continuous(trans = "log10")
+
+
+## drying
+
+release_drying = 
+  wrc_processed %>% 
+  select(vol_water_perc, treatment, texture, kPa) %>% 
+  # keep only the SCL drying data
+  filter(treatment %in% "drying") %>% 
+  # calculate wet pore size using the Kelvin equation
+  mutate(pore_um = round(300/kPa,2)) %>% 
+  # for repeating kPa/pore, calculate mean of water content
+  group_by(pore_um, texture, treatment) %>% 
+  dplyr::summarise(vol_water_perc = round(mean(vol_water_perc),2)) %>% 
+  ungroup() %>% 
+  # then create bins in increments of 10 (0-3000, 300 breakpoints)
+  mutate(x_bins = cut(pore_um, breaks = 300)) %>% 
+  # split the bins into start and stop columns
+  separate(x_bins, c("start", "stop"), sep = ",") %>% 
+  mutate(stop = str_replace(stop, "]",""),
+         stop = as.numeric(stop),
+         start = stri_replace_all_fixed(start, "(",""),
+         #         start = str_replace(start, "-",""),
+         start = as.numeric(start)) %>%
+  group_by(texture) %>% 
+  arrange(desc(pore_um)) %>% 
+  ungroup()
+
+release_drying_diff = 
+  release_drying %>% 
+  group_by(texture) %>% 
+  # calculate sequential loss of water (lag)
+  mutate(Diff = (vol_water_perc - lag(vol_water_perc))*-1) %>% 
   na.omit() 
 
-ggplot(release_scl_wetting_bins2, aes(x = (pore_um), y = Diff_perc))+
-  geom_point()+
-  scale_x_continuous(trans = "log10", labels = scales::comma)+
-  labs(title = "scl")
+# ^^  this leaves the lowest value as the residual water (0um pore), not included in the diff
+#     so create a new file with just the residual moisture, and then join it with the diff file
 
-
-####
-
-release_sl_wetting = 
-  wrc_processed %>% 
-  select(vol_water_perc, treatment, texture, kPa) %>% 
-  filter(treatment %in% "wetting" & texture %in% "SL") %>% 
-  mutate(pore_um = round(300/kPa,2),
-         x_bins = cut(pore_um, breaks = 300)) %>% 
-  separate(x_bins, c("start", "stop"), sep = ",") %>% 
-  mutate(stop = str_replace(stop, "]",""),
-         stop = as.numeric(stop),
-         start = stri_replace_all_fixed(start, "(",""),
-         #         start = str_replace(start, "-",""),
-         start = as.numeric(start)
-  ) 
-
-
-release_sl_wetting_bins2 = 
-  release_sl_wetting %>% 
+release_drying_residual = 
+  release_drying_diff %>% 
+  group_by(texture) %>% 
+  filter(vol_water_perc==min(vol_water_perc)) %>% 
   ungroup() %>% 
-  arrange((vol_water_perc)) %>% 
-  mutate(Diff = vol_water_perc - lag(vol_water_perc),
-         Diff_perc = (Diff/max(vol_water_perc))*100)  %>% 
-  na.omit()
+  mutate(Diff = vol_water_perc)
 
-ggplot(release_sl_wetting_bins2, aes(x = (pore_um), y = Diff_perc))+
-  geom_point()+
-  scale_x_continuous(trans = "log10", labels = scales::comma)+
-  labs(title = "sl")
+# now merge
+release_drying_diff2 = 
+  release_drying_diff %>% 
+  rbind(release_drying_residual) %>% 
+  group_by(texture) %>% 
+  mutate(Diff_perc = round((Diff/max(vol_water_perc))*100,2))
 
+release_drying_diff2 %>%  
+  ggplot()+
+  geom_point(aes(x = pore_um, y = Diff))
 
-release_sl_wetting_summary  = 
-  release_sl_wetting %>% 
+release_drying_summary  = 
+  release_drying_diff2 %>% 
   group_by(start, stop, treatment, texture) %>% 
-  summarize(vol_water = max(vol_water_perc)) 
+  summarize(Diff = sum(Diff), # cm3
+            Diff_perc = sum(Diff_perc)) 
 
-
-release_sl_wetting_bins = 
-  release_sl_wetting_summary %>% 
+release_drying_bins = 
+  release_drying_summary %>% 
   ungroup() %>% 
-  # arrange(desc(stop)) %>% 
-  mutate(Diff = vol_water - lag(vol_water),
-         Diff_perc = (Diff/max(vol_water))*100)  %>% 
-  na.omit() %>% 
-  #  mutate(bins = case_when(start==50 & stop==100 ~ "50-100",
-  #                          start>=100 & start <250 ~ "100-250",
-  #                          start >=250 ~ "> 250")) %>% 
+  arrange(desc(stop)) %>% 
   mutate(bins = if_else(stop<=100, stop, 3000)) %>% 
-  group_by(bins) %>% 
-  summarize(water_vol_perc = round(sum(Diff_perc),2))
+  group_by(treatment, texture, bins) %>% 
+  dplyr::summarise(vol_water_cm3 = sum(Diff),
+                   vol_water_perc = sum(Diff_perc))
 
+release_drying_bins %>% 
+  ggplot(aes(x = reorder(as.character(bins), bins), y = vol_water_cm3, color = texture))+
+  geom_point(size=3.5)+
+  labs(x = "pore size class, upper limit (um)",
+       y = "volume of pores in size class (cm3)")+
+  scale_color_manual(values = soilpalettes::soil_palette("eutrostox",2))+
+  theme_kp()+
+  NULL
 
-bins_10_sl = tribble(
-  ~bins, ~water_vol_perc,
-  10, (52.57/max(release_sl_wetting_summary$vol_water))*100
-)
-## < 10 um = 37.43 = 51.54 % of total volume
-
-bins_combined_sl = 
-  release_sl_wetting_bins %>% 
-  rbind(bins_10_sl) %>% 
-  arrange(bins) %>% 
-  mutate(texture="SL")
-
-ggplot(bins_combined_sl, aes(x = (bins), y = water_vol_perc))+
-  geom_point()
-
-
-bins_combined_both = 
-  bins_combined %>% 
-  mutate(texture="SCL") %>% 
-  rbind(bins_combined_sl) %>% 
-  arrange(bins)
-
-bins_combined_both %>% 
-  ggplot(aes(x = reorder(as.character(bins),bins), y = water_vol_perc, color = texture))+
+release_drying_diff2 %>% 
+  mutate(mask = if_else(Diff>5, "large", "small")) %>% 
+  ggplot(aes(x = pore_um, y = Diff, color = texture))+
   geom_point()+
-  geom_path(aes(group=texture))
+  labs(x = "pore size (um)",
+       y = "volume of pores (cm3)")+
+  scale_color_manual(values = soilpalettes::soil_palette("eutrostox",2))+
+  #facet_grid(mask~., scales = "free_y")+
+  scale_y_log10(breaks=c(0.1, 0.2, 0.5, 1, 5, 10))+
+  theme_kp()+
+  theme(panel.grid.minor = element_blank())+
+  NULL
 
+release_drying_summary %>% 
+  #mutate(mask = if_else(Diff>5, "large", "small")) %>% 
+  ggplot(aes(x = stop, y = Diff, color = texture))+
+  geom_point()+
+  labs(x = "pore size (um)",
+       y = "volume of pores (cm3)",
+       subtitle = "pore size in 10 um bins")+
+  scale_color_manual(values = soilpalettes::soil_palette("eutrostox",2))+
+  #facet_grid(mask~., scales = "free_y")+
+  scale_y_log10(breaks=c(0.1, 0.2, 0.5, 1, 5, 10))+
+  scale_x_log10()+
+  theme_kp()+
+  theme(panel.grid.minor = element_blank())+
+  NULL
+
+  
 
 
 
